712a8410c57617d35ac17cdc4159e8e80101883d
blukoo <dnehdrkd123@naver.com>
2022-03-13 23:04:41 +0900

    feat 단위 변환기

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+function MinutesToHours() {
+  const [amounts, setAmounts] = React.useState(0);
+  const [inverted, setInverted] = React.useState(false);
+  const onChange = event => {
+    setAmounts(event.target.value);
+  };
+  const reset = () => setAmounts(amounts * 0);
 
-function App() {
-  const [counter, setCounter] = React.useState(0);
-  const onClick = () => {
-    setCounter(current => current + 1);
+  const onFlip = () => {
+    reset();
+    setInverted(current => !current);
   };
   return (
     <div>
-      <h3>Total click : {counter}</h3>
-      <button onClick={onClick}>Click me</button>
+      <label htmlFor="minutes">Minutes</label>
+      <input
+        onChange={onChange}
+        disabled={inverted}
+        id="minutes"
+        value={inverted ? amounts * 60 : amounts}
+        placeholer="Minutes"
+        type="number"
+      />
+      <h4>You want to convert {amounts}</h4>
+      <label htmlFor="hours">Hours</label>
+      <input
+        onChange={onChange}
+        id="hours"
+        disabled={!inverted}
+        value={inverted ? amounts : Math.round(amounts / 60)}
+        placeholer="Hours"
+        type="number"
+      />
+      <button onClick={reset}>리셋</button>
+      <button onClick={onFlip}>{inverted ? 'turn off' : 'inverted'}</button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Super Converter</h1>
+      <MinutesToHours></MinutesToHours>
     </div>
   );
 }
