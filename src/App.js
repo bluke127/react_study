70966543551e46392ff46ea283d92b3dd8cad137
blukoo <dnehdrkd123@naver.com>
2022-03-17 22:04:20 +0900

    feat 무비 api 연결 및 route 설치

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
 import { useState, useEffect } from 'react';
-
+import Movie from './components/Movie';
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch('https://api.coinpaprika.com/v1/tickers')
-      .then((data) => data.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-  return (
-    <div>
-      <h1>The conins</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((item, index) => (
-          <li key={index}>{item.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return null;
 }
 
 export default App;
